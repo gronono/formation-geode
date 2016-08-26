56011deac7eca5de9ac1f38cc238aa5c52acbc17
Arnaud Brunet <arnaud.brunet+github@gmail.com>
2016-08-27 00:29:52 +1100

    ajout du composant liste-offres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ListeOffresComponent } from './liste-offres.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    ListeOffresComponent
+  ]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/liste-offres.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'liste-offres',
+  templateUrl: 'liste-offres.component.html',
+  styleUrls: ['liste-offres.component.css'],
+})
+export class ListeOffresComponent {
+
+}
