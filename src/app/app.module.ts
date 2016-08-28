1d05a13fa41a445f4c5f1b2262e16f3ad209908d
Arnaud Brunet <arnaud.brunet+github@gmail.com>
2016-08-28 22:18:53 +1100

    fix rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {CountPipe} from "./CountPipe";
+import {CountPipe} from "./count.pipe";
 
 @NgModule({
   declarations: [
