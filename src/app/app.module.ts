cdb7a38ec03e78d0cc990a15411216904269f6a9
Arnaud Brunet <arnaud.brunet+github@gmail.com>
2016-08-28 21:49:09 +1100

    ajout d'un pipe pour compter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {CountPipe} from "./CountPipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountPipe
   ],
   imports: [
     CommonModule,
