f3805353e30d55be8d42ec6933d290e67a9a55dc
Arnaud Brunet <arnaud.brunet+github@gmail.com>
2016-08-30 01:09:28 +1100

    piloté par le template

---
+++ src/app/offres/offre-form/offre-form.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from '@angular/core';
+import {Offre} from '../index';
+
+@Component({
+  moduleId: module.id,
+  selector: 'geode-detail-offre',
+  templateUrl: 'detail-offre.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DetailOffreComponent {
+
+  @Input() offre: Offre;
+  @Output() addFavoris = new EventEmitter<Offre>();
+
+  onAddFavoris() {
+    this.addFavoris.emit(this.offre);
+  }
+}
